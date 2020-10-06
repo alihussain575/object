75878f013f8b9701283d516a099cda63fd6cf5b8
ali hussain <71531323+alihussain575@users.noreply.github.com>
2020-10-06 16:02:36 -0700

    this is the task related to objects assinged by ilyas

---
+++ script.js
@@ -0,0 +1,348 @@
+// const john ={
+//     name = "john",
+//     chris :{
+//         name= "chris",
+//         ali:{
+//             name = "ali"
+//         },
+//         karim:{
+//             name="karim",
+//         }
+//     },
+//     alex :{
+//         john :{
+//             name="john",
+//             ali:{
+//                 name="ali",
+//                 suleiman:{
+//                     name="suleiman",
+//                     kareem:{
+//                         name="kareem",
+//                     },
+//                     raheem:{
+//                         name="raheem",
+//                         kaleem:{
+//                             name="kaleem",
+//                             amir:{
+//                                 name="amir",
+//                             },
+//                             liaqat:{
+//                                 name="liaqat"
+//                             },
+//                         }
+//                     }
+//                 },
+//                 usmn:{
+//                     name="usman"
+//                 },
+//                 salman:{
+//                     name="salman"
+//                 },
+//                 javed:{
+//                     name="javed"
+//                 },
+//             },
+//             jack:{
+//                 name="jack",
+//             }
+//         },
+//         alex:{
+//             name="alex"
+//         },
+//         garret:{
+//             name="Garret"
+//         },
+//         wood:{
+//             name="wood",
+//             chris:{
+//                 name="chris" 
+//             },
+//             roy:{
+//                 name="Roy"
+//             },
+//             max:{
+//                 name="Max",
+//                 usman:{
+//                     name="usman"
+//                 },
+//                 salman:{
+//                     name="salman"
+//                 },
+//                 javed:{
+//                     name="javed"
+//                 },
+//                 mark:{
+//                     name="mark"
+//                 },
+//                 alex:{
+//                     name="alex"
+//                 }
+//             },
+//             nova:{
+//                 name="Nova"
+//             },
+//             root:{
+//                 name="Root"
+//             },
+//             mark:{
+//                 name="mark",
+//                 morgan:{
+//                     name="Morgan"
+//                 },
+//                 watson:{
+//                     name="Watson"
+//                 }
+//             }
+//         },
+//         hales:{
+//             name="Hales"
+//         },
+//     },
+//     jack:{
+//         name="jack",
+//         kevin:{
+//             name="kevin"
+//         },
+//         derekhood:{
+//             name="derekhood"
+//         },
+//     },
+//     david:{
+//         name="David",
+//         kegan:{
+//             name="Kagen",
+//             alex:{
+//                 name="Alex",
+//                 jackma:{
+//                     name="jackma",
+//                     kevin:{
+//                         name="kevin"
+//                     },
+//                     derek:{
+//                         name="derek"
+//                     },
+//                     trump:{
+//                         name="trump"
+//                     },
+//                     obama:{
+//                         name="obama"
+//                     },
+//                     bush:{
+//                         name="bush"
+//                     }
+//                 },
+//                 bill:{
+//                     name="Bill",
+//                     robert:{
+//                         name="robert"
+//                     },
+//                     john:{
+//                         name="john"
+//                     }
+//                 }
+//             }
+//         }
+//     }
+    
+// }
+// console.log([name]);
+// document.write(john)
+// let family ={
+//     name="john",
+//     john:{
+//         firstson="Chris",
+//         secondson="Alex",
+//         chris:{
+//             firstson="Ali",
+//             secondson="karim",
+//         },
+//         Alex:{
+//             firstson="john",
+//             secondson="Alex",
+//             thridson= "Garret",
+//             fourthson= "Wood",
+//             fifthson ="Hales",
+//             john2:{
+//                 firstson="Ali",
+//                 secondson="Jack",
+//                 Ali:{
+//                     firstson="suleiman",
+//                     secondson="usman",
+//                     thridson= "salman",
+//                     fourthson= "javed",
+//                     suleiman:{
+//                         firstson="kareem",
+//                         secondson="raheem",
+//                         reaheem:{
+//                             firstson="nadeem",
+//                             secondson="kaleem",
+//                             kaleem:{
+//                                 firstson="Amir",
+//                                 secondson="Liaqat",
+//                             }
+//                         }
+//                     }
+//                 }
+//             },
+//             wood:{
+//                 firstson="Chris",
+//                 secondson="Roy",
+//                 thridson= "Max",
+//                 fourthson= "Nova",
+//                 fifthson="Root",
+//                 sixthson="mark",
+//                 Max:{
+//                     firstson="Usman",
+//                     secondson="Salman",
+//                     thridson= "Javed",
+//                     fourthson= "Mark",
+//                     fifthson="Alex",
+//                 },
+//                 Mark:{
+//                     firstson="Morgan",
+//                     secondson="Watson",
+//                 }
+//             }
+//         },
+
+//     }
+// }
+// let family2={
+//     name="jack",
+//     jack:{
+//         firstson="kevin",
+//         secondson="DerekWood",
+//     }
+// }
+let John = {
+    son1: "Chris <br/>",
+    Chris: {
+      son1: "Ali <br/>",
+      son2: "Karim <br/>",
+    },
+    son2: "Alex <br/>",
+    Alex: {
+      son1: "John <br/>",
+      John: {
+        son1: "Ali <br/>",
+        Ali: {
+          son1: "Suleiman <br/>",
+          Suleiman: {
+            son1: "Kareem <br/>",
+            son2: "Raheem <br/>",
+            Raheem: {
+              son1: "Nadeem <br/>",
+              son2: "Kaleem <br/>",
+              Kaleem: {
+                son1: "Amir <br/>",
+                son2: "Liaqat <br/>",
+              },
+            },
+          },
+          son2: "Usman <br/>",
+          son3: "Salman <br/>",
+          son4: "Javed <br/>",
+        },
+        son2: "Jack <br/>",
+      },
+      son2: "Alex <br/>",
+      son3: "Garret <br/>",
+      son4: "Wood <br/>",
+      Wood: {
+        son1: "Chris <br/>",
+        son2: "Roy <br/>",
+        son3: "Max <br/>",
+        Max: {
+          son1: "Usman <br/>",
+          son2: "Salman <br/>",
+          son3: "Javed <br/>",
+          son4: "Mark <br/>",
+          son5: "Alex <br/>",
+        },
+        son4: "Nova <br/>",
+        son5: "Root <br/>",
+        son6: "Mark <br/>",
+        Mark: {
+          son1: "Morgan <br/>",
+          son2: "Watson v",
+        },
+      },
+      son5: "Hales <br/>",
+    },
+  };
+  
+  // family 2
+  
+  let Jack = {
+    son1: "Kevin <br/>",
+    son2: "DerekWood <br/>",
+  };
+  
+  // family 3
+  
+  let David = {
+    son1: "Kegan <br/>",
+    Kegan: {
+      son1: "Alex <br/>",
+      Alex: {
+        son1: "Jackma <br/>",
+        Jackma: {
+          son1: "Kevin <br/>",
+          son2: "Derek <br/>",
+          son3: "Trump <br/>",
+          son4: "Obama <br/>",
+          son5: "Bush <br/>",
+        },
+        son2: "Bill <br/>",
+        Bill: {
+          son1: "Robert <br/>",
+          son2: "John <br/>",
+        },
+      },
+    },
+  };
+  console.log(John);
+    document.write(John.Alex.John.Ali.Suleiman.son1 )
+    document.write(John.Alex.John.Ali.Suleiman.son2)
+//   document.write(John);
+  console.log(Jack);
+  Jack.son2="DerekKhan";
+//   document.write(Jack)
+  console.log(David);
+  David.Kegan.Alex.Jackma.son3="Donald Trump";
+  delete David.Kegan.Alex.Jackma.son4;
+  delete David.Kegan.Alex.Jackma.son5;
+//   document.write(David);
+function printValues(obj) {
+    for (var key in obj) {
+        if (typeof obj[key] != "object") {
+            document.write(obj[key]); 
+               
+        } else {
+            printValues(obj[key]);  
+        }
+    }
+}
+printValues(John)
+function printValues(obj) {
+    for (var key in obj) {
+        if (typeof obj[key] != "object ") {
+            document.write(obj[key]); 
+               
+        } else {
+            printValues(obj[key]);  
+        }
+    }
+}
+printValues(Jack)
+function printValues(obj) {
+    for (var key in obj) {
+        if (typeof obj[key] != "object") {
+            document.write(obj[key]); 
+               
+        } else {
+            printValues(obj[key]);  
+        }
+    }
+}
+printValues(David)
